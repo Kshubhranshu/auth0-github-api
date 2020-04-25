13bfc86849be1078bb0b621186e5a0241a27c12d
kshubhranshu <shubhranshu14@gmail.com>
2020-04-25 22:11:49 +0530

    Updates

---
+++ precache-manifest.05f939acd6bc843dddb7852a79001465.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20deb7aa6bb02a68814b016ac03d88b",
+    "url": "/auth0-github-api/index.html"
+  },
+  {
+    "revision": "57eb2642d810c97fa9a2",
+    "url": "/auth0-github-api/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e2f82320ff7af45a4a3f",
+    "url": "/auth0-github-api/static/css/main.b0cb1113.chunk.css"
+  },
+  {
+    "revision": "57eb2642d810c97fa9a2",
+    "url": "/auth0-github-api/static/js/2.a58973b8.chunk.js"
+  },
+  {
+    "revision": "d91d38a8ad4f79373a620f3b9674daa4",
+    "url": "/auth0-github-api/static/js/2.a58973b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f82320ff7af45a4a3f",
+    "url": "/auth0-github-api/static/js/main.9ad30934.chunk.js"
+  },
+  {
+    "revision": "f48ed338b9f271c3c293",
+    "url": "/auth0-github-api/static/js/runtime-main.cbd1b679.js"
+  },
+  {
+    "revision": "db0721b19bf1b047866172744342786f",
+    "url": "/auth0-github-api/static/media/logo.db0721b1.svg"
+  }
+]);
